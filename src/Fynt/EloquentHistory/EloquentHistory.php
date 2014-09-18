9e46ca838805acaa409338b2de427b55e2263a8c
Ryan Leland <ryanleland@icloud.com>
2014-09-18 21:37:44 +0000

    Adds history trait.

--- src/Fynt/EloquentHistory/EloquentHistory.php
+++ src/Fynt/EloquentHistory/EloquentHistory.php
@@ -2,4 +2,15 @@
 
 class EloquentHistory {
 
+  /**
+   * Get the table name, where we will save the versions from the Laravel config.
+   *
+   * @return Illuminate\Database\Query\Builder
+   */
+  public static function getVersionsTable()
+  {
+    $tableName = Config::get('eloquent-history::table');
+    return DB::table($tableName);
+  }
+
 }
