712e58a009f0b1e12d4b75b9f1c82dd754d7e90c
Ryan Leland <ryanleland@icloud.com>
2014-09-18 18:51:58 +0000

    Adds config and migration.

---
+++ src/migrations/2014_09_18_174251_create_history_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistoryTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('history', function($table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned()->index();
+			$table->string('action', 30);
+			$table->integer('object_id')->unsigned();
+			$table->string('object_table', 64)->index();
+			$table->timestamps();
+
+			$table->index(array('object_id', 'object_table'));
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('history');
+	}
+
+}
