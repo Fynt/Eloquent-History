d67938a68986551566a447262af6604c279e60c1
Ryan Leland <ryanleland@gmail.com>
2014-09-18 11:43:14 -0600

    Adds model observer.

---
+++ src/Fynt/EloquentHistory/ModelObserver.php
@@ -0,0 +1,17 @@
+<?php namespace Fynt\EloquentHistory;
+
+class ModelObserver {
+
+  public function created($model)
+  {
+  }
+
+  public function updated($model)
+  {
+  }
+
+  public function deleted($model)
+  {
+  }
+
+}
