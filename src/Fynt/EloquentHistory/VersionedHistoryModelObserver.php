d350a5341f9417b9a692819d56edf501c6049c2a
Ryan Leland <ryanleland@icloud.com>
2014-09-26 19:40:12 +0000

    Adds publish as  history action.

--- src/Fynt/EloquentHistory/VersionedHistoryModelObserver.php
+++ src/Fynt/EloquentHistory/VersionedHistoryModelObserver.php
@@ -2,16 +2,23 @@
 
 use \Auth;
 use \History;
+use \Versions;
 
 class VersionedHistoryModelObserver {
 
   const ACTION_CREATE = 'create';
+  const ACTION_PUBLISH = 'publish';
   const ACTION_UPDATE = 'update';
   const ACTION_DELETE = 'delete';
 
   public function created($model)
   {
     History::register(Auth::user(), self::ACTION_CREATE, $model);
+  }
+
+  public function published($model)
+  {
+    History::register(Auth::user(), self::ACTION_PUBLISH, $model); 
   }
 
   public function updated($model)
--- src/Fynt/EloquentHistory/VersionedHistoryTrait.php
+++ src/Fynt/EloquentHistory/VersionedHistoryTrait.php
@@ -1,6 +1,10 @@
 <?php namespace Fynt\EloquentHistory;
 
+use Fynt\EloquentVersions\VersionedTrait;
+
 trait VersionedHistoryTrait {
+
+  use VersionedTrait;
 
   public static function bootVersionedHistoryTrait()
   {
