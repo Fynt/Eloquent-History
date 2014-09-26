2937dfc088f33868288fdfe44caf0c1573bc2369
Ryan Leland <ryanleland@gmail.com>
2014-09-26 11:19:37 -0600

    Start of versioned history.

--- src/migrations/2014_09_18_174251_create_history_table.php
+++ src/migrations/2014_09_18_174251_create_history_table.php
@@ -5,34 +5,34 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateHistoryTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('history', function($table)
-		{
-			$table->increments('id');
-			$table->integer('user_id')->unsigned()->nullable()->index();
-			$table->string('action', 30);
-			$table->integer('object_id')->unsigned();
-			$table->string('object_table', 64)->index();
-			$table->timestamps();
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('history', function($table)
+    {
+      $table->increments('id');
+      $table->integer('user_id')->unsigned()->nullable()->index();
+      $table->string('action', 30);
+      $table->integer('object_id')->unsigned();
+      $table->string('object_table', 64)->index();
+      $table->timestamps();
 
-			$table->index(array('object_id', 'object_table'));
-		});
-	}
+      $table->index(array('object_id', 'object_table'));
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('history');
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('history');
+  }
 
 }
